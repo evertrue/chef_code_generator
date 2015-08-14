7733d3ae6eca70d77ad250e01435597b139a6db4
Jeff Byrnes <jeff@evertrue.com>
2015-08-14 15:36:11 -0400

    Update name to match checked out name
    The cookbook name needs to match the path used by ChefDK/Knife when
    using `chef generate`.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,7 @@
-name             'code_generator'
+name             'generator_cookbook'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'dev@chef.io'
 license          'Apache 2 License'
 description      'Generates Chef code for Chef DK'
 long_description 'Generates Chef code for Chef DK'
 version          '0.1.0'
-
